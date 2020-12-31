a35ba691bb594610c6fdc4dc6d2fbec509ddc964
OzlemNET <OzlemNET@DESKTOP-IUS183R>
2021-01-01 00:30:36 +0300

    Proje dosyası ekle.

---
+++ geometrikhesapla/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace geometrikhesapla
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            label1 = new Label();
+            panel2 = new Panel();
+            dairesonuc = new Label();
+            textBox1 = new TextBox();
+            yarıcaplabel = new Label();
+            pictureBox1 = new PictureBox();
+            dikdortgensonuc = new Label();
+            panel3 = new Panel();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            kısaklabel = new Label();
+            uzunk4 = new Label();
+            pictureBox2 = new PictureBox();
+            panel4 = new Panel();
+            label2 = new Label();
+            karesonuc = new Label();
+            textBox4 = new TextBox();
+            kenar5 = new Label();
+            pictureBox3 = new PictureBox();
+            button1 = new Button();
+            label4 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(255, 128, 0);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1015, 100);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Rounded MT Bold", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Yellow;
+            label1.Location = new Point(268, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(419, 37);
+            label1.TabIndex = 0;
+            label1.Text = " GEOMETRİ UYGULAMASI";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(255, 255, 128);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(dairesonuc);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(yarıcaplabel);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(12, 151);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(237, 491);
+            panel2.TabIndex = 1;
+            // 
+            // dairesonuc
+            // 
+            dairesonuc.AutoSize = true;
+            dairesonuc.Location = new Point(107, 453);
+            dairesonuc.Name = "dairesonuc";
+            dairesonuc.Size = new Size(43, 15);
+            dairesonuc.TabIndex = 3;
+            dairesonuc.Text = "Sonuç:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(89, 363);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // yarıcaplabel
+            // 
+            yarıcaplabel.AutoSize = true;
+            yarıcaplabel.Location = new Point(14, 366);
+            yarıcaplabel.Name = "yarıcaplabel";
+            yarıcaplabel.Size = new Size(48, 15);
+            yarıcaplabel.TabIndex = 1;
+            yarıcaplabel.Text = "Yarıçap:";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(14, 69);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(196, 271);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // dikdortgensonuc
+            // 
+            dikdortgensonuc.AutoSize = true;
+            dikdortgensonuc.Location = new Point(148, 453);
+            dikdortgensonuc.Name = "dikdortgensonuc";
+            dikdortgensonuc.Size = new Size(43, 15);
+            dikdortgensonuc.TabIndex = 4;
+            dikdortgensonuc.Text = "Sonuç:";
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(128, 255, 128);
+            panel3.Controls.Add(label3);
+            panel3.Controls.Add(dikdortgensonuc);
+            panel3.Controls.Add(textBox3);
+            panel3.Controls.Add(textBox2);
+            panel3.Controls.Add(kısaklabel);
+            panel3.Controls.Add(uzunk4);
+            panel3.Controls.Add(pictureBox2);
+            panel3.Location = new Point(365, 151);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(259, 491);
+            panel3.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(16, 453);
+            label3.Name = "label3";
+            label3.Size = new Size(106, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Dikdörtgenin Alanı";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(111, 401);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(111, 363);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // kısaklabel
+            // 
+            kısaklabel.AutoSize = true;
+            kısaklabel.Location = new Point(29, 366);
+            kısaklabel.Name = "kısaklabel";
+            kısaklabel.Size = new Size(64, 15);
+            kısaklabel.TabIndex = 3;
+            kısaklabel.Text = "Kısa Kenar:";
+            // 
+            // uzunk4
+            // 
+            uzunk4.AutoSize = true;
+            uzunk4.Location = new Point(29, 401);
+            uzunk4.Name = "uzunk4";
+            uzunk4.Size = new Size(70, 15);
+            uzunk4.TabIndex = 4;
+            uzunk4.Text = "Uzun Kenar:";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(29, 91);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(201, 249);
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.Cyan;
+            panel4.Controls.Add(label2);
+            panel4.Controls.Add(karesonuc);
+            panel4.Controls.Add(textBox4);
+            panel4.Controls.Add(kenar5);
+            panel4.Controls.Add(pictureBox3);
+            panel4.Location = new Point(760, 151);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(267, 491);
+            panel4.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(22, 453);
+            label2.Name = "label2";
+            label2.Size = new Size(77, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Karenin Alanı";
+            // 
+            // karesonuc
+            // 
+            karesonuc.AutoSize = true;
+            karesonuc.Location = new Point(120, 453);
+            karesonuc.Name = "karesonuc";
+            karesonuc.Size = new Size(43, 15);
+            karesonuc.TabIndex = 5;
+            karesonuc.Text = "Sonuç:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(89, 363);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 7;
+            // 
+            // kenar5
+            // 
+            kenar5.AutoSize = true;
+            kenar5.Location = new Point(22, 366);
+            kenar5.Name = "kenar5";
+            kenar5.Size = new Size(40, 15);
+            kenar5.TabIndex = 2;
+            kenar5.Text = "Kenar:";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(37, 91);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(201, 232);
+            pictureBox3.TabIndex = 0;
+            pictureBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Red;
+            button1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            button1.ForeColor = SystemColors.Control;
+            button1.Location = new Point(12, 661);
+            button1.Name = "button1";
+            button1.Size = new Size(1028, 38);
+            button1.TabIndex = 3;
+            button1.Text = "HESAPLA";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(14, 453);
+            label4.Name = "label4";
+            label4.Size = new Size(84, 15);
+            label4.TabIndex = 4;
+            label4.Text = "Dairenin Alanı:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 192, 255);
+            ClientSize = new Size(1052, 788);
+            Controls.Add(button1);
+            Controls.Add(panel3);
+            Controls.Add(panel4);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private Label dikdortgensonuc;
+        private Label dairesonuc;
+        private TextBox textBox1;
+        private Label yarıcaplabel;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Label kısaklabel;
+        private Label uzunk4;
+        private Label karesonuc;
+        private TextBox textBox4;
+        private Label kenar5;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ geometrikhesapla/Form1.cs
@@ -0,0 +1,24 @@
+namespace geometrikhesapla
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int kenar=Convert.ToInt32(textBox4.Text);
+            int ksonuc = kenar * kenar;
+            karesonuc.Text = ksonuc.ToString();
+            int kısak=Convert.ToInt32(textBox2.Text);
+            int uzunk = Convert.ToInt32(textBox3.Text);
+            int dsonuc = kısak * uzunk;
+            dikdortgensonuc.Text = dsonuc.ToString();
+            double yaricap=Convert.ToDouble(textBox1.Text);
+            double dairealan = Math.PI * (yaricap * yaricap);
+            dairesonuc.Text = dairealan.ToString();
+        }
+    }
+}
